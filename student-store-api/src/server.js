d189339583a7706aba6f76f9b6b50de9773cad12
Jaqueline Jurado <jaqueline.jurado@jaqueli-ltmdbv3.internal.salesforce.com>
2025-06-23 12:02:38 -0700

    Initial commit including product model and product CRUD ops

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -1,1 +1,26 @@
 
+require("dotenv").config(); 
+const express = require('express');
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+const productRoutes = require("../routes/productRoutes")
+
+
+const corsOption = {
+    origin: "https://localhost:5173",
+// change this link to the front end link
+}
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/product", productRoutes)
+
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
